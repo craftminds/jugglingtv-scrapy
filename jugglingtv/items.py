f9be401dc1fc8b11da5754fcaf1cfbf986f4614c
Artur <artwitek@gmail.com>
2022-02-06 09:31:51 +0100

    Working with nested links

--- jugglingtv/items.py
+++ jugglingtv/items.py
@@ -3,10 +3,24 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class JugglingtvItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class VideoItem(Item):
+    # title = Field()
+    # video_link = Field()
+    # thumbnail = Field()
+    # views = Field()
+    # duration = Field()
+    # author = Field()
+    # comments_no = Field()
+    # video_description = Field()
+    # video_country = Field()
+    # video_channels = Field()
+    # video_tags = Field()
+    # video_added_date = Field()
     pass
+
+
+
+
