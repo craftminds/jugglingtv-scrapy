47f4f4269d5e060657879d7fd2d4bbf2ac30d34d
Artur <artwitek@gmail.com>
2022-02-07 00:28:36 +0100

    Half working - work on the input_processor

--- jugglingtv/items.py
+++ jugglingtv/items.py
@@ -4,23 +4,36 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 from scrapy.item import Item, Field
+from scrapy.loader.processors import MapCompose, TakeFirst
 
 
+def remove_white_spaces(text):
+    # strip from tabs and spaces
+    text = text.re("[^\t\n]+")
+    return text
+
+# should do some polishing on the output info
 class VideoItem(Item):
-    # title = Field()
-    # video_link = Field()
-    # thumbnail = Field()
-    # views = Field()
-    # duration = Field()
-    # author = Field()
-    # comments_no = Field()
-    # video_description = Field()
-    # video_country = Field()
-    # video_channels = Field()
-    # video_tags = Field()
-    # video_added_date = Field()
-    pass
+    title = Field(
+        input_processor=MapCompose(remove_white_spaces),
+        output_processor=TakeFirst()
+        )
+    video_link = Field()
+    thumbnail = Field()
+    views = Field()
+    duration = Field()
+    author = Field()
+    comments_no = Field()
+    video_description = Field()
+    video_year = Field()
+    video_country = Field()
+    video_channels = Field()
+    video_tags = Field()
+   
+    
 
+
+# continue here:https://towardsdatascience.com/a-minimalist-end-to-end-scrapy-tutorial-part-ii-b917509b73f7
 
 
 
--- jugglingtv/spiders/juggling_spider.py
+++ jugglingtv/spiders/juggling_spider.py
@@ -7,36 +7,42 @@ from scrapy.loader import ItemLoader
 
 class AuthorSpider(scrapy.Spider):
     name = 'author'
-    # video_item = VideoItem()
+    video_item = VideoItem()
 
     start_urls = ['http://juggling.tv/videos/basic/mr']
 
     def parse(self, response):
         for video in response.css("div.listwatch"):
-            yield {
-                'title': video.css('table.title a::text').re("[^\t\n]+")[0],
-                'video_link': video.css('table.title a::attr(href)').get(),
-                'thumbnail': video.css('div.imagewatch img::attr(src)').get(),
-                'views': video.css('td.views::text').get(),
-                'duration': video.css('td.duration::text').re("[^\t\n]+")[0],
-                'author': video.css('td.by a::text').get(),
-                'comments_no': video.css('td.comments::text').get(),
-            }
+            loader = ItemLoader(item=VideoItem(), selector = video)
+            loader.add_css('title', 'table.title a::text')
+            loader.add_css('video_link', 'table.title a::attr(href)')
+            video_item = loader.load_item()
+                #  'title': video.css('table.title a::text').re("[^\t\n]+")[0],
+                # 'video_link': video.css('table.title a::attr(href)').get(),
+                # 'thumbnail': video.css('div.imagewatch img::attr(src)').get(),
+                # 'views': video.css('td.views::text').get(),
+                # 'duration': video.css('td.duration::text').re("[^\t\n]+")[0],
+                # 'author': video.css('td.by a::text').get(),
+                # 'comments_no': video.css('td.comments::text').get(),
+
+            # get inside the video and extract data
             video_url = video.css('table.title a::attr(href)').get()
-            self.logger.info('get video page url')
-            self.logger.info(video_url)
-            yield response.follow(video_url, callback = self.parse_single_video)
+            yield response.follow(video_url, self.parse_single_video, meta={'video_item': video_item})
 
         next_page = response.css('a.rightPaging::attr(href)').get()
-        if next_page is 'http://juggling.tv/videos/basic/mr/3':
+        if next_page is not 'http://juggling.tv/videos/basic/mr/3':
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
     
     def parse_single_video(self, response):
         # def extract_with_css(query):
         #     return response.css(query).get(default='').strip()
-        
-        yield {
-            'video_title':response.css('h3.vv-video-title::text').get(),
-        }
+       video_item = response.meta['video_item']
+       loader = ItemLoader(item=video_item, response = response)
+       loader.add_css('video_description', 'div.vv-video-desc::text')
+       loader.add_css('video_year', 'span.vv-loco::text')
+       loader.add_css('video_country', 'span.vv-cunt::text')
+       loader.add_css('video_channels', 'div.mb-5.vv-chan a::attr(href)')
+       loader.add_css('video_tags', 'div.mb-5.vv-tags a::attr(href)')
 
+       
