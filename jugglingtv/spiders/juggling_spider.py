cd04326fddb1ced2273b0eada75b9ac5264113b2
Artur <artwitek@gmail.com>
2022-02-06 09:32:33 +0100

    Nested url

--- jugglingtv/spiders/juggling_spider.py
+++ jugglingtv/spiders/juggling_spider.py
@@ -1,8 +1,13 @@
 import scrapy
+from jugglingtv.items import VideoItem
+from scrapy.loader import ItemLoader
+
+
 
 
 class AuthorSpider(scrapy.Spider):
     name = 'author'
+    # video_item = VideoItem()
 
     start_urls = ['http://juggling.tv/videos/basic/mr']
 
@@ -17,8 +22,21 @@ class AuthorSpider(scrapy.Spider):
                 'author': video.css('td.by a::text').get(),
                 'comments_no': video.css('td.comments::text').get(),
             }
+            video_url = video.css('table.title a::attr(href)').get()
+            self.logger.info('get video page url')
+            self.logger.info(video_url)
+            yield response.follow(video_url, callback = self.parse_single_video)
 
         next_page = response.css('a.rightPaging::attr(href)').get()
-        if next_page is not None:
+        if next_page is 'http://juggling.tv/videos/basic/mr/3':
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+    
+    def parse_single_video(self, response):
+        # def extract_with_css(query):
+        #     return response.css(query).get(default='').strip()
+        
+        yield {
+            'video_title':response.css('h3.vv-video-title::text').get(),
+        }
+
